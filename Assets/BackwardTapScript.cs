b50a537757bdf86de106c1b1d85aafcea2981646
JonathanTF <jonny28f@gail.com>
2018-01-27 14:16:06 -0600

    new branch

---
+++ Assets/BackwardTapScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackwardTapScript : MonoBehaviour
+{
+
+    public Button B;
+    public hamMovement move;
+    public GameObject hamster;
+
+    void Start()
+    {
+        Button btn = B.GetComponent<Button>();
+        if(btn == null)
+        btn.onClick.AddListener(TaskOnClick);
+        move = hamster.GetComponent<hamMovement>();
+    }
+
+    void TaskOnClick()
+    {
+        print("You have clicked the backward button!");
+        move.tapBackward();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
