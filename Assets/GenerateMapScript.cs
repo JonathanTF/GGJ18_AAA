a58b957907ef5350d198cf763f07db5c82813102
Albangalo <Albert.chen.nz@gmail.com>
2018-01-27 13:15:32 -0600

    New levels load upon reaching finish point
    - imported 4 levels

--- Assets/GenerateMapScript.cs
+++ Assets/GenerateMapScript.cs
@@ -20,10 +20,24 @@ public class GenerateMapScript : MonoBehaviour {
     const int WIDTH = 11;
     string[] lines;
 
+    int level;
+
 	// Use this for initialization
 	void Start () {
-        LoadMaze("Mazes/maze1");
+        level = 1;
+        LoadMaze("Mazes/maze" + level);
 	}
+
+    public void FinishLevel()
+    {
+        // increment level, destroy level, load next level
+        level++;
+        foreach (Transform child in transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+        LoadMaze("Mazes/maze" + level);
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -55,15 +69,25 @@ public class GenerateMapScript : MonoBehaviour {
             {
                 char tileChar = lines[i][j];
 
+                
+
                 switch (tileChar){
                     case (WALL_CHAR):
                         GameObject wall = Instantiate<GameObject>(Wall, new Vector3(i, 0, j) * WALL_SIZE, Quaternion.identity);
                         wall.transform.localScale = Vector3.one * WALL_SIZE;
+                        wall.transform.parent = gameObject.transform;
                         break;
                     case (START_CHAR):
-                        Instantiate<GameObject>(StartTile, new Vector3(i, -0.5f, j) * WALL_SIZE, Quaternion.AngleAxis(90, Vector3.right));
-                        Instantiate<GameObject>(Harry, new Vector3(i, 0, j) * WALL_SIZE, Quaternion.identity); break;
-                    case (FIN_CHAR): Instantiate<GameObject>(Finish, new Vector3(i, 0, j) * WALL_SIZE, Quaternion.identity); break;
+                        GameObject start = Instantiate<GameObject>(StartTile, new Vector3(i, -0.5f, j) * WALL_SIZE, Quaternion.AngleAxis(90, Vector3.right));
+                        GameObject harry = Instantiate<GameObject>(Harry, new Vector3(i, 0, j) * WALL_SIZE, Quaternion.identity); 
+                        start.transform.parent = gameObject.transform;
+                        harry.transform.parent = gameObject.transform;
+                        break;
+                    case (FIN_CHAR):
+                        GameObject finish = Instantiate<GameObject>(Finish, new Vector3(i, 0, j) * WALL_SIZE, Quaternion.identity);
+                        finish.transform.parent = gameObject.transform;
+                        break;
+
                     default: break;
                 }
 
