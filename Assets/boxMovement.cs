c36effac2e9eae7c280de969358c6e05737f9a6f
JonathanTF <jonny28f@gail.com>
2018-01-27 11:03:12 -0600

    initial commit

---
+++ Assets/boxMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boxMovement : MonoBehaviour {
+
+    public bool move = true;
+    public float velocity = 10;
+    public float turnVelocity = 10;
+    Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+
+        rb = GetComponent<Rigidbody>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        if (move)
+        {
+             
+            rb.velocity = transform.forward * Input.GetAxis("Vertical") * velocity;
+
+            transform.Rotate(Vector3.up, Input.GetAxis("Horizontal"));
+
+        }
+    }
+}
