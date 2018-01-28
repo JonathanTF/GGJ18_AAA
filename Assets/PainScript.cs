af7a7cb70c0c7934a680a8544417b98232b8d711
stonji <stonji@me.com>
2018-01-28 12:20:53 -0600

    Paincript edit

--- Assets/PainScript.cs
+++ Assets/PainScript.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class PainScript : MonoBehaviour {
 
-    const float ZAP_MAX = 2f;
+    const float ZAP_MAX = 1.3f;
     const float ZAP_MIN = 1f;
 	AudioSource as_zap;
     float zapness;
+	float zap_delay = 0.25f;
 
     public void Zap()
     {
         if (zapness < ZAP_MAX)
         {
-            zapness += 0.15f;
-            zapness *= 1.2f;
+            //zapness += 0.15f;
+            zapness *= 1.04f;
+			zap_delay = 0.25f;
             Debug.Log("Ouch! zapness: " + zapness);
         }
     }
@@ -28,14 +30,16 @@ public class PainScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (zapness > ZAP_MIN)
+
+		zap_delay -= 0.01f;
+		if (zapness > ZAP_MIN && zap_delay <= 0.0f)
         {
-            zapness -= 0.01f;
+            zapness -= 0.03f;
+			Debug.Log("Ouch! zapness: " + zapness);
         }
-        if (zapness > ZAP_MAX)
-        {
-            Debug.Log("Too much ZAP!!!");
-        }
+		if (zapness > ZAP_MAX) {
+			Debug.Log ("Too much ZAP!!!");
+		}
 		as_zap.pitch = zapness;
 	}
 }
